0cd7111494dfa5f3748c6f7608b9879b18c5d0a6
codeman6100 <cody.gerhartz@gmail.com>
2019-09-26 10:12:55 -0500

    include user modification in zshrc and font installation

--- zshsetup.sh
+++ zshsetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Installing Dependencies
 sudo apt install zsh git -y
 echo Dependencies installed.
 #echo Configure Shell
@@ -10,9 +11,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 cp -f ./.zshrc  ~/
+sed -i 's/gerhartz/$USER/g' ~/.zshrc
 cp ./.bash_aliases ~/
 
 #echo Install a Nerd Font
 # you can modify this if you want a different font.
-#git clone https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/RobotoMono/Regular
-
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Roboto Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+fc-cache -f
+echo You must change the font of your termianl to the install Roboto Mono font in order for glyphs to work.
