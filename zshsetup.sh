cab1264b9bd8401133cbf24b7066b3db5ee3c1f8
codeman6100 <gerhartz@Universe.ads.ssc.wisc.edu>
2019-09-30 09:34:21 -0500

    restart terminal text and exit ohmyzsh

--- zshsetup.sh
+++ zshsetup.sh
@@ -6,6 +6,7 @@ echo Dependencies installed.
 #chsh --shell /etc/zsh
 echo Configuring Zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+exit
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
@@ -20,3 +21,4 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Roboto Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
 fc-cache -f
 echo You must change the font of your termianl to the install Roboto Mono font in order for glyphs to work.
+echo Restart your terminal for changes to take effect.
