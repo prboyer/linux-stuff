ea3d607dbc0f6d045785dc2cb32a2d232bb701d5
Paul Boyer <prboyer@users.noreply.github.com>
2019-10-18 11:17:12 -0500

    FONTS .. plz download and install properly

--- zshsetup.sh
+++ zshsetup.sh
@@ -2,9 +2,11 @@
 echo Installing Dependencies
 sudo apt install zsh git -y
 echo Dependencies installed.
-#echo Configure Shell
-#chsh --shell /etc/zsh
-echo Configuring Zsh
+
+echo Configure Shell
+chsh --shell /bin/zsh
+
+echo Configuring ZSH
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -14,11 +16,15 @@ cp -f ./.zshrc  ~/
 sed -i 's/pboyer2/$USER/g' ~/.zshrc
 cp ./.bash_aliases ~/
 
-#echo Install a Nerd Font
+echo Install a Nerd Font
 # you can modify this if you want a different font.
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Roboto Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+wget ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf 
+
 fc-cache -f
+
 echo You must change the font of your termianl to the installed Roboto Mono font in order for glyphs to work.  Or comment out the "nerd-font" line in your ~/.zshrc file
 echo Restart your terminal for changes to take effect.
+
 reset
