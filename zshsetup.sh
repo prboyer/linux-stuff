5bbfe72ec41e6ce5ce7bde19c6fb5a496aeb4dcb
Paul Boyer <prboyer@users.noreply.github.com>
2019-10-21 09:18:04 -0500

    troubleshooting fonts

--- zshsetup.sh
+++ zshsetup.sh
@@ -20,18 +20,18 @@ cp ./.bash_aliases ~/
 echo Install a Nerd Font
 # you can modify this if you want a different font.
 mkdir -p ~/.local/share/fonts
-#cd ~/.local/share/fonts && curl -fLo "Roboto Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
-#wget ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf 
+cd ~/.local/share/fonts && curl -fLo "Roboto Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+wget ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf 
 
-echo Install PowerLine Font
+#echo Install PowerLine Font
 # instrucitons here: https://powerline.readthedocs.io/en/latest/installation/linux.html#fonts-installation
-cd ~/.local/share/fonts
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+#cd ~/.local/share/fonts
+#wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 fc-cache -vf ~/.local/share/fonts/
 
 echo Install PowerLine Symbols
-mkdir -p ~/.config/fontconfig/conf.d
-cd ~/.config/fontconfig/conf.d/ && wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+#mkdir -p ~/.config/fontconfig/conf.d
+#cd ~/.config/fontconfig/conf.d/ && wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
 echo You must change the font of your termianl to the installed Roboto Mono font in order for glyphs to work.  Or comment out the "nerd-font" line in your ~/.zshrc file
 echo Restart your terminal for changes to take effect.
