58f57bf3c1dfbb2bd1f4dea534a5947832104192
Paul Boyer @ Tomahawk <prboyer@sbcglobal.net>
2019-10-31 14:35:23 -0500

    added a script to apply WSL settings

---
+++ WSL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ./WSLSettings >> ~/.zshrc
+
+# Apppends the folling to the end of the .zshrc file
+#Setting GUI Display for WSL
+#export DISPLAY=:0
+
+#open home directory for WSL
+#cd
