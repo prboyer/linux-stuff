ae9fb6526884d6c7f26baa442b9dfc2a39c68d3a
Paul Boyer <pboyer2@wisc.edu>
2021-01-13 13:01:33 -0600

    started revision process for 2021. Deleted old packages from the repo. Re-arragned the .zshrc file.

---
+++ old/setup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+echo Install Git
+sudo apt install cowsay
+cowthink "how'd you clone the repo without git?"
+
+echo Install Etcher
+echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee
+/etc/apt/sources.list.d/etcher.list
+
+Trust Bintray.com GPG key
+$ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+
+echo Install Updates
+sudo apt update && sudo apt upgrade -y
+
+echo Finish Installing Etcher
+sudo apt install etcher-electron -y
+
+echo Remove Firefox
+sudo apt remove firefox -y 
+
+echo Install OpenSSH
+sudo apt install openssh-server -y
+
+echo Install Net Tools
+sudo apt install net-tools -y
+
+echo Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo Install VSCode
+# see https://code.visualstudio.com/docs/setup/linux for more information
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+
+# trying to install via snap
+sudo snap install --classic code 
+
+echo Install Remmina Remote Desktop
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next -y
+sudo apt update
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice -y
+
+echo Install PaloAlto GlobalProtect
+tar -xvzf ./PanGPLinux-4.1.9-c3.tgz
+sudo dpkg -i ./*.deb
+# post-instal cleanup
+rm -rf ./GlobalProtect*
+rm ./manifest
+rm ./relinfo
+
+echo Install PowerShell
+# Download the Microsoft repository GPG keys
+wget -q ./ https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Update the list of products
+sudo apt-get update
+
+# Enable the "universe" repositories
+sudo add-apt-repository universe
+
+# Install PowerShell
+sudo apt-get install -y powershell
+
+#if fails try via snap
+# Install PowerShell
+sudo snap install powershell --classic
+
+# install GParted
+sudo apt install GParted -y
+
+# install Microsoft Teams
+sudo snap install teams-for-linux
+# make a link to the teams binary
+sudo ln -s -T /snap/bin/teams-for-linux /snap/bin/teams
+
+echo Cleanup
+rm -f packages*
+
+# Copy custom user scripts
+cp ./usr_bin/* /usr/bin
+sudo rm /usr/bin/TEMPLATE
+sudo rm /usr/bin/README
+
+# run the ZSH installation
+echo Install ZSH
+sudo sh ./zshsetup.sh
--- old/zshsetup.sh
+++ old/zshsetup.sh
@@ -11,24 +11,29 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 cp -f ./.zshrc  ~/
 sed -i 's/pboyer2/$USER/g' ~/.zshrc
-#cp ./.bash_aliases ~/
-
+cp ./.bash_aliases ~/
+cp ./.zsh_theme_var ~/
 
 echo Install a Nerd Font
 # you can modify this if you want a different font.
 mkdir -p ~/.local/share/fonts
 chmod 755 ~/.local/share/fonts
-cp RobotoMonoNerdFontCompleteMono.ttf ~/.local/share/fonts
+cp ./RobotoMonoNerdFontCompleteMono.ttf ~/.local/share/fonts
 chmod 644 ~/.local/share/fonts/*
 fc-cache -vf ~/.local/share/fonts/
+
+# copy theme var file
+cp ./.zsh_theme_var ~
 
 # copy themes to local folder
 echo copying themes
 cp ./themes/* ~/.oh-my-zsh/themes
 
-# change the shell to ZSH
-echo Changing Shell
-chsh -s /bin/zsh
-
 echo You must change the font of your termianl to the installed Roboto Mono font in order for glyphs to work.  Or comment out the "nerd-font" line in your ~/.zshrc file
 echo Restart your terminal for changes to take effect.
+
+reset
+
+#echo Changing Shell
+#chsh -s /bin/zsh
+zsh
